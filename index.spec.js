b6da7af8beb41af6f985360f10b76c01fad8f9e1
Ilya Kaminsky <ilyakam@gmail.com>
2016-12-17 18:33:09 -0800

    feat(index): show a daily Dilbert comic
    Show a daily Dilbert comic by sending a Slack message to the hidden
    incoming webhook URL with the desired option at 15:00 UTC.

--- index.spec.js
+++ index.spec.js
@@ -1,11 +1,30 @@
 var mockery = require('mockery');
 
-describe('schedule', function() {
+describe('index', function() {
   var mockNodeSchedule,
-      spyOnScheduleJob;
+      spyOnMockSlackNotify,
+      spyOnScheduleJob,
+      spyOnSlackNotifySend;
 
   beforeEach(function() {
-    spyOnScheduleJob = jasmine.createSpy('nodeSchedule.scheduleJob');
+    var date = new Date(2015, 9, 4);
+
+    jasmine.clock().install();
+
+    jasmine.clock().mockDate(date);
+
+    spyOnScheduleJob = jasmine
+      .createSpy('nodeSchedule.scheduleJob')
+      .and.callFake(function(cron, callback) {
+        callback();
+      });
+
+    spyOnSlackNotifySend = jasmine
+      .createSpy('slackNotify.send');
+
+    spyOnMockSlackNotify = jasmine
+      .createSpy('slackNotify')
+      .and.returnValue({send: spyOnSlackNotifySend});
 
     mockNodeSchedule = {scheduleJob: spyOnScheduleJob};
 
@@ -15,18 +34,41 @@ describe('schedule', function() {
     });
 
     mockery.registerMock('node-schedule', mockNodeSchedule);
+    mockery.registerMock('slack-notify', spyOnMockSlackNotify);
 
     require('./index.js');
   });
 
   afterEach(function() {
+    jasmine.clock().uninstall();
+
     mockery.disable();
 
     mockery.deregisterAll();
   });
 
-  it('should fire a cron job every day', function() {
-    expect(spyOnScheduleJob)
-      .toHaveBeenCalledWith('asdf');
+  describe('node-schedule', function() {
+    it('should be set up', function() {
+      expect(spyOnScheduleJob)
+        .toHaveBeenCalledWith(jasmine.any(String), jasmine.any(Function));
+    });
+  });
+
+  describe('slack-notify', function() {
+    it('should be set up', function() {
+      expect(spyOnMockSlackNotify)
+        .toHaveBeenCalled();
+    });
+
+    it('should send a message', function() {
+      expect(spyOnSlackNotifySend)
+        .toHaveBeenCalledWith({
+          channel: jasmine.any(String),
+          icon_url: jasmine.any(String),
+          text: jasmine.stringMatching('2015-10-04'),
+          unfurl_links: jasmine.any(Boolean),
+          username: jasmine.any(String)
+        });
+    });
   });
 });
